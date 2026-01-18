95077dad12bdc3482bc574ed50610ac6543631b1
lajczi <lajczi@disroot.org>
2026-01-18 16:37:13 +0100

    Fix logic

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const time = Date.now();
+console.log(`[Meteor] launching bot`);
 
-console.log(`[${process.env.HOSTNAME || 'meteor'}] launching bot`);
+const client = './charmer/client.js';
 
-// Get the correct path for each environment type
-let client = './charmer/client.js';
-if (process.env.PATH_OVERRIDE) client = process.env.PATH_OVERRIDE;
-
-// Import and run the client directly
+// Run the client
 require(client);
-
-console.log(`[${process.env.HOSTNAME || 'meteor'}] bot ready (${Date.now() - time}ms)`);
